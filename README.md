d197f3ddb1af430de37112af32df2239eae10b34
Sapein <5852983+Sapein@users.noreply.github.com>
2026-03-06 10:51:49 -0600

    Update to Bevy 0.18, remove deprecated functionality, update documentation.

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ To use this crate you just need to insert the `LinkWindowToMonitorPlugin`, after
 and add a relationship onto the Window specifying which Monitor the Window is on. After which you can use the `OnMonitor`
 and `HasWindows` Relationship Components to determine which window it's on.
 
-You may also use `determine_monitor()` which will return the Monitor we determine the window to be on, if we can determine it,
-if you wish to handle the linking yourself.
 
 ## Bevy Version Compatibility
 | Link Window to Monitor | Bevy Version |
@@ -16,11 +14,12 @@ if you wish to handle the linking yourself.
 |          0.2           |     0.16     |
 |        0.3-rc.1        |  0.17-rc.1   |
 |          0.3           |     0.17     |
+|          0.4           |     0.18     |
 
 ## Example
 Here's a brief example of a very basic Bevy App that, alongside this crate, will create a window and print out the monitor
 it is on.
-```rust,ignore
+```rust
 use bevy::prelude::*;
 use bevy::window::{Monitor, PrimaryWindow};
 use bevy_link_window_to_monitor::LinkWindowToMonitorPlugin;
@@ -36,14 +35,3 @@ fn print_monitor(primary_window: Single<&OnMontior, With<PrimaryWindow>>, monito
   println!("{:?}", monitors.iter().find(|(e, ..)| *e == primary_window.0).unwrap().1.name)
 }
 ```
-
-## Known Edge Cases
-If you want to use `determine_monitor()`, you may encounter some edge-cases where the function is unable to determine
-the Monitor the Window is on, or otherwise experience a delay, such as:
-- If you set `window.position` to `WindowPosition::Automatic` then detecting the Monitor may take up to one frame.
-- If you set `window.position` to `WindowPosition::Centered` with `MonitorSelection::Curent` or `MonitorSelection::Index`
-  then monitor detection may take up to one frame.
-- Detection can completely fail if you continuously set `window.position` or do it immediately before the linking system
-  runs or immediately after the `changed_windows` system provided by Bevy.
-
-The plugin manages to get access to the underlying Winit Windows and is able to get the monitor the window is on that way.
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use bevy_ecs::component::Component;
 use bevy_ecs::entity::Entity;
 use bevy_ecs::query::{Changed, Or, With};
 use bevy_ecs::system::{Commands, NonSend, Query};
-use bevy_window::{Monitor, MonitorSelection, PrimaryMonitor, Window, WindowPosition};
+use bevy_window::{Monitor, Window};
 use bevy_winit::WinitWindows;
 
 /// The main plugin that, when added, will automagically update and manage Window positions.
@@ -23,85 +23,6 @@ pub struct OnMonitor(pub Entity);
 #[derive(Component, Debug)]
 #[relationship_target(relationship = OnMonitor, linked_spawn)]
 pub struct HasWindows(Vec<Entity>);
-
-/// Attempt to use heuristics to determine the which monitor the window is likely on. This may,
-/// however, fail under certain circumstances.
-///
-/// # Arguments
-///
-/// * `window`: The Window that you're trying to find which monitor it's on.
-/// * `monitors`: A vector of all known monitors, with some additional data to make it easier to find.
-///
-/// returns: Option<&Monitor>
-///
-/// # Examples
-///
-/// ```
-/// use bevy_ecs::entity::Entity;
-/// use bevy_ecs::query::With;
-/// use bevy_ecs::system::{Query, Single};
-/// use bevy_window::{Monitor, PrimaryMonitor, PrimaryWindow, Window};
-///
-/// use bevy_link_window_to_monitor::determine_monitor;
-///
-/// fn which_monitor(window: Single<&Window, With<PrimaryWindow>>, monitors: Query<(&Monitor, Option<&PrimaryMonitor>, Entity)>) {
-///     if let Some(monitor) = determine_monitor(*window, monitors.iter().collect()) {
-///         println!("On Monitor: {}", monitor.name.clone().unwrap());
-///     } else {
-///         println!("No monitor found!");
-///     }
-/// }
-/// ```
-#[deprecated(
-    since = "0.2.0",
-    note = "Use the System Parameter NonSend<WinitWindows> to use the winit window to get the current monitor instead."
-)]
-pub fn determine_monitor<'a>(
-    window: &Window,
-    monitors: Vec<(&'a Monitor, Option<&PrimaryMonitor>, Entity)>,
-) -> Option<&'a Monitor> {
-    match window.position {
-        WindowPosition::Automatic => None,
-        WindowPosition::At(window_pos) => {
-            let mut zero_monitor = None;
-            for (monitor, ..) in monitors {
-                let extents = [
-                    monitor.physical_position,
-                    monitor.physical_position + monitor.physical_size().as_ivec2(),
-                ];
-
-                if monitor.physical_position.x == 0 && monitor.physical_position.y == 0 {
-                    zero_monitor = Some(monitor);
-                }
-
-                if window_pos.x >= extents[0].x
-                    && window_pos.x <= extents[1].x
-                    && window_pos.y >= extents[0].y
-                    && window_pos.y <= extents[1].y
-                {
-                    return Some(monitor);
-                }
-            }
-            if window_pos.x < 0 || window_pos.y < 0 {
-                zero_monitor
-            } else {
-                None
-            }
-        }
-        WindowPosition::Centered(selection) => match selection {
-            MonitorSelection::Current => None, // We can not actually determine position.
-            MonitorSelection::Index(_) => None,
-            MonitorSelection::Primary => monitors
-                .into_iter()
-                .find(|(_, p, ..)| p.is_some())
-                .map(|(m, ..)| m),
-            MonitorSelection::Entity(entity) => monitors
-                .into_iter()
-                .find(|(.., e)| *e == entity)
-                .map(|(m, ..)| m),
-        },
-    }
-}
 
 fn link_monitor_to_window(
     mut commands: Commands,
